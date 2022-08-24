706d55d1d8083c9ffe20c08929c35e83d1e37511
Gregory <grigory.art223@gmail.com>
2022-08-24 21:09:29 +0300

    v1.0

---
+++ service/genre_service.py
@@ -0,0 +1,14 @@
+from dao.model.genre import Genre
+from dao.genre_dao import GenreDAO
+
+
+class GenreService:
+
+    def __init__(self, genre_dao: GenreDAO):
+        self.genre_dao = genre_dao
+
+    def get_genres(self) -> list[Genre]:
+        return self.genre_dao.get_all_genres()
+
+    def get_genre_by_id(self, id):
+        return self.genre_dao.get_genre_by_id(id)
